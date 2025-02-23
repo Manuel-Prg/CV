0891255104e4e0d32915bf6d1484206d2b9b9a5f
ManuelPrz-Tech <134172077+Manuel-Prg@users.noreply.github.com>
2025-02-23 13:35:18 -0600

    add theme dark

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#4B5563',
+          dark: '#E5E7EB',
+        },
+      },
+    },
   },
   plugins: [],
 }
-
